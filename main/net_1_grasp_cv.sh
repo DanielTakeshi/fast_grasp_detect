8659433312be8677eb8c37191df972fe52904187
DanielTakeshi <takeshidanny@gmail.com>
2018-08-16 16:58:52 -0700

    lets run CV on h_v02 but with net 3:

--- main/net_1_grasp_cv.sh
+++ main/net_1_grasp_cv.sh
@@ -1,5 +1,5 @@
 
 for (( i=0; i<10; i++ )) ; do
-    python main/train_bed_grasp.py --do_cv --cv_idx $i --max_iters 500 --lrate 0.0001 --net_type 1
+    python main/train_bed_grasp.py --do_cv --cv_idx $i --max_iters 2500 --lrate 0.0001 --net_type 1
 done
 
