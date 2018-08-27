ac8b74fc206bbf9140102c312f9c8d43720f844b
DanielTakeshi <takeshidanny@gmail.com>
2018-08-27 08:22:08 -0700

    standardize scripts:

---
+++ main/README.md
@@ -0,0 +1,5 @@
+# Scripts
+
+Run these like: `./main/script_name.sh`.
+
+I use 6000 iterations for the `combo` data, and 4000 iterations for individual data.
--- main/net_3_grasp_cv.sh
+++ main/net_3_grasp_cv.sh
@@ -1,6 +1,6 @@
 
 for (( i=0; i<10; i++ )) ; do
     python main/train_bed_grasp.py --do_cv --cv_idx $i \
-            --max_iters 3000 --lrate 0.0001 --net_type 3
+            --max_iters 6000 --lrate 0.0001 --net_type 3
 done
 
