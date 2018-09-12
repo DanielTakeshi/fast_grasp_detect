b14ab1b4c9e52de72d03c0c6858f9b003b5641b7
DanielTakeshi <takeshidanny@gmail.com>
2018-09-11 20:18:28 -0700

    newer datset to work

--- main/net_1_grasp_cv.sh
+++ main/net_1_grasp_cv.sh
@@ -1,6 +1,6 @@
 
-for (( i=0; i<10; i++ )) ; do
+for (( i=1; i<10; i++ )) ; do
     python main/train_bed_grasp.py --do_cv --cv_idx $i \
-            --max_iters 8001 --lrate 0.0001 --net_type 1
+            --max_iters 5000 --lrate 0.0001 --net_type 1
 done
 
